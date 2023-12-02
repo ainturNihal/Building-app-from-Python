8f24aff8f2e335521060e483d4f7933cc81d43ca
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-02 17:54:21 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import Gridlayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.bitton import Button
