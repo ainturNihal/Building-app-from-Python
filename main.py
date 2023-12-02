aa45feccf7a1e91915358a986bc448ed87f10d6a
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-02 17:59:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from kivy.uix.gridlayout import Gridlayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.bitton import Button
+
+class childApp(Gridlayout):
+  def __init__(self,**kwargs):
+    super(childApp,self).__init__()
+    self.cols = 2
+    
